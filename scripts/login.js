3c0262455358624959a31820a585a3f6effaa4d8
Sidiney Ramos <sidineyramos123@gmail.com>
2023-11-23 11:49:07 -0300

    php

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+/*function logar(){
+    var usuario = document.getElementById('user').value;
+    var senha = document.getElementById('password').value;
+
+    if(usuario =="admin" && senha == "123") {
+        alert("Login efetuado!");
+        location.href= "index.php"
+    }
+    else{
+        alert("Usuario ou senha incorreto!");
+    }
+}*/
